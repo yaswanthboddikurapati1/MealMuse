52f523125b8d08930e61aa6a42ac4c240044adb1
Shujad Abdulla <shujadabdulla12@gmail.com>
2025-07-31 08:20:45 +0000

    const firebaseConfig = {   apiKey: "AIzaSyARm7TZJNih9r7eGnkuvAHouSK3clw0

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp, getApps } from "firebase/app";
 
 const firebaseConfig = {
-  "projectId": "mealmuse-f74k5",
-  "appId": "1:833047350413:web:ad67bd70df93bf5403e6c4",
-  "storageBucket": "mealmuse-f74k5.firebasestorage.app",
-  "apiKey": "AIzaSyARm7TZJNih9r7eGnkuvAHouSK3clw0ibk",
-  "authDomain": "mealmuse-f74k5.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "833047350413"
+  apiKey: "AIzaSyARm7TZJNih9r7eGnkuvAHouSK3clw0ibk",
+  authDomain: "mealmuse-f74k5.firebaseapp.com",
+  projectId: "mealmuse-f74k5",
+  storageBucket: "mealmuse-f74k5.firebasestorage.app",
+  messagingSenderId: "833047350413",
+  appId: "1:833047350413:web:ad67bd70df93bf5403e6c4",
+  measurementId: ""
 };
 
 // Initialize Firebase
